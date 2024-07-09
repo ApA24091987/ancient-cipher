8f7702f2fd6900f7c7f09cc53890d6cf27b00de2
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-07-09 20:43:31 +0300

    Update DLC.py

--- DLC.py
+++ DLC.py
@@ -1,15 +1,13 @@
 def find_password(n):
     result = ''
     for i in range(1, n):
-        for j in range(i, n+1):
-            if (i + j) % n == 0:
+        for j in range(i +1, n+1):
+            if (n % (i + j)) == 0:
                 result += str(i) + str(j)
-
     return result
 
-n = int(input("введите число от 3 до 20: "))
+n = int(input("Введите число от 3 до 20: "))
 if 3 <= n <= 20:
     print(find_password(n))
 else:
     print("Некорректное значение. Введите число от 3 до 20.")
-#ответ число 11.
